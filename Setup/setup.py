bcd4c71741041a935823f9e5109e7903d66ff324
cshuo <cshuo@cshuo.com>
2016-01-11 03:50:12 -0500

    cshuo: oslo.config版本不同, 修改setup.py文件,

--- Setup/setup.py
+++ Setup/setup.py
@@ -2,30 +2,30 @@ __author__ = 'pike'
 
 from setuptools import setup
 
-driverGroup = 'oslo.messaging.drivers'
+driverGroup = 'oslo_messaging.drivers'
 
 transportDrivers = [
-    'rabbit = oslo.messaging._drivers.impl_rabbit:RabbitDriver',
-    'qpid = oslo.messaging._drivers.impl_qpid:QpidDriver',
-    'zmq = oslo.messaging._drivers.impl_zmq:ZmqDriver',
+    'rabbit = oslo_messaging._drivers.impl_rabbit:RabbitDriver',
+    'qpid = oslo_messaging._drivers.impl_qpid:QpidDriver',
+    'zmq = oslo_messaging._drivers.impl_zmq:ZmqDriver',
 
     # To avoid confusion
-    'kombu = oslo.messaging._drivers.impl_rabbit:RabbitDriver',
+    'kombu = oslo_messaging._drivers.impl_rabbit:RabbitDriver',
 
     # For backwards compat
     'openstack.common.rpc.impl_kombu ='
-    ' oslo.messaging._drivers.impl_rabbit:RabbitDriver',
+    ' oslo_messaging._drivers.impl_rabbit:RabbitDriver',
     'openstack.common.rpc.impl_qpid ='
-    ' oslo.messaging._drivers.impl_qpid:QpidDriver',
+    ' oslo_messaging._drivers.impl_qpid:QpidDriver',
     'openstack.common.rpc.impl_zmq ='
-    ' oslo.messaging._drivers.impl_zmq:ZmqDriver',
+    ' oslo_messaging._drivers.impl_zmq:ZmqDriver',
 ]
 
-executorGroup = 'oslo.messaging.executors'
+executorGroup = 'oslo_messaging.executors'
 
 executors = [
-    'blocking = oslo.messaging._executors.impl_blocking:BlockingExecutor',
-    'eventlet = oslo.messaging._executors.impl_eventlet:EventletExecutor'
+    'blocking = oslo_messaging._executors.impl_blocking:BlockingExecutor',
+    'eventlet = oslo_messaging._executors.impl_eventlet:EventletExecutor'
 
 ]
 
@@ -37,34 +37,3 @@ setup(
     name = "oslo_driver",
     entry_points = entry_points
 )
-
-#[entry_points]
-#console_scripts =
-#    oslo-messaging-zmq-receiver = oslo.messaging._cmd.zmq_receiver:main
-#
-#oslo.messaging.drivers =
-#    rabbit = oslo.messaging._drivers.impl_rabbit:RabbitDriver
-#    qpid = oslo.messaging._drivers.impl_qpid:QpidDriver
-#    zmq = oslo.messaging._drivers.impl_zmq:ZmqDriver
-#    amqp = oslo.messaging._drivers.protocols.amqp:ProtonDriver
-#
-#    # To avoid confusion
-#    kombu = oslo.messaging._drivers.impl_rabbit:RabbitDriver
-#
-#    # This is just for internal testing
-#    fake = oslo.messaging._drivers.impl_fake:FakeDriver
-#
-#oslo.messaging.executors =
-#    blocking = oslo.messaging._executors.impl_blocking:BlockingExecutor
-#    eventlet = oslo.messaging._executors.impl_eventlet:EventletExecutor
-#
-#oslo.messaging.notify.drivers =
-#    messagingv2 = oslo.messaging.notify._impl_messaging:MessagingV2Driver
-#    messaging = oslo.messaging.notify._impl_messaging:MessagingDriver
-#    log = oslo.messaging.notify._impl_log:LogDriver
-#    test = oslo.messaging.notify._impl_test:TestDriver
-#    noop = oslo.messaging.notify._impl_noop:NoOpDriver
-#    routing = oslo.messaging.notify._impl_routing:RoutingDriver
-#
-#oslo.config.opts =
-#    oslo.messaging = oslo.messaging.opts:list_opts
