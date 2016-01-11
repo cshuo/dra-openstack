e7ae3e9f16bdf9023b73eab4b69532f7cf37c158
cshuo <cshuo@cshuo.com>
2016-01-11 04:01:02 -0500

    修改针对openstack系统的配置信息

--- Openstack/Conf/OpenstackConf.py
+++ Openstack/Conf/OpenstackConf.py
@@ -1,8 +1,8 @@
 __author__ = 'pike'
 
-CONTROLLER_HOST = "114.212.189.134"
-COMPUTE1_HOST = "114.212.189.139"
-COMPUTE2_HOST = "114.212.189.133"
+CONTROLLER_HOST = "20.0.1.11"
+COMPUTE1_HOST = "20.0.1.21"
+COMPUTE2_HOST = "20.0.1.31"
 #NETWORKING_HOST = ""
 CEILOMETER_HOST = CONTROLLER_HOST
 
@@ -10,26 +10,26 @@ AUTH_URL = "http://%s:35357" % CONTROLLER_HOST
 NOVA_URL = "http://%s:8774" % CONTROLLER_HOST
 CEILOMETER_URL = "http://%s:8777" % CEILOMETER_HOST
 
-HOST_USERNAME = "nju"
-HOST_PASSWORD = "njuics08"
+HOST_USERNAME = "vagrant"
+HOST_PASSWORD = "cshuo"
 
 HOST_ROOT_USERNAME = "root"
-HOST_ROOT_PASSWORD = "njuics08"
+HOST_ROOT_PASSWORD = "cshuo"
 
 COMPUTE1_HOST_USERNAME = "root"
-COMPUTE1_HOST_PASSWORD = "cs"
+COMPUTE1_HOST_PASSWORD = "cshuo"
 
 COMPUTE2_HOST_USERNAME = "root"
-COMPUTE2_HOST_PASSWORD = "cs"
+COMPUTE2_HOST_PASSWORD = "cshuo"
 
 
 USERNAME = "admin"
 TENANTNAME = "admin"
-PASSWORD = "ADMIN_PASS"
+PASSWORD = "cshuo"
 
 # environment variables
 PARAMS = "--os-username %s " % USERNAME + \
          "--os-tenant-name %s " % TENANTNAME + \
-         "--os-auth-url %s/v2.0 " % AUTH_URL + \
+         "--os-auth-url %s/v3 " % AUTH_URL + \
          "--os-password %s" % PASSWORD
 
